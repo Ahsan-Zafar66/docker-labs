826e25639219bd8f1c2eedf5b426feac80165830
ahsan zafar <ahsanzafar0501@gmail.com>
2025-08-19 06:06:19 +0500

    Changin app code

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello from Node.js App deployed via CodeBuild & ECS!');
+    res.send('Hello from Node.js App deployed via CodeBuild & ECS!. Now I am modifying my code');
 });
 
 app.listen(PORT, () => {
